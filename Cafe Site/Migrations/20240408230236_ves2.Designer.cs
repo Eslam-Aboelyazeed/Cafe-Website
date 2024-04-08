184be58066c69994280e5c4a33927b5ee2a24914
Eslam-Aboelyazeed <eslamaboelyazeed22@gmail.com>
2024-04-09 01:08:26 +0200

    Adjusting Contact Page and Adding Product Reviews Migration Commit

--- Cafe Site/Migrations/20240408230236_ves2.Designer.cs
+++ Cafe Site/Migrations/20240408230236_ves2.Designer.cs
@@ -4,6 +4,7 @@ using Cafe_Site.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Cafe_Site.Migrations
 {
     [DbContext(typeof(CafeSiteContext))]
-    partial class CafeSiteContextModelSnapshot : ModelSnapshot
+    [Migration("20240408230236_ves2")]
+    partial class ves2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
